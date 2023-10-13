90913493aaac4f75b5552cfddefefdd1e4390a76
Joseph Perez <joseph.perez@numberly.com>
2023-10-13 10:07:13 +0200

    refactor: remove `#[pyclass]` from async generator implem

--- src/async_generator.rs
+++ src/async_generator.rs
@@ -1,7 +1,7 @@
 use std::{
     marker::PhantomData,
-    mem,
     pin::Pin,
+    sync::{Arc, Mutex},
     task::{ready, Context, Poll},
 };
 
@@ -9,11 +9,10 @@ use pyo3::{exceptions::PyStopAsyncIteration, prelude::*};
 
 use crate::{PyFuture, PyStream, ThrowCallback};
 
-#[pyclass]
-struct StreamNext(Option<Pin<Box<dyn PyStream>>>);
+type SharedStream = Arc<Mutex<Option<Pin<Box<dyn PyStream>>>>>;
 
 struct PyStreamNext {
-    next: Py<StreamNext>,
+    stream: SharedStream,
     close: bool,
 }
 
@@ -21,41 +20,19 @@ impl PyFuture for PyStreamNext {
     fn poll_py(self: Pin<&mut Self>, py: Python, cx: &mut Context) -> Poll<PyResult<PyObject>> {
         let err = || Err(PyStopAsyncIteration::new_err(py.None()));
         let this = Pin::into_inner(self);
-        let mut ref_mut = this.next.borrow_mut(py);
-        let Some(stream) = &mut ref_mut.0 else {
+        let mut guard = this.stream.lock().unwrap();
+        let Some(ref mut stream) = *guard else {
             return Poll::Ready(err());
         };
         let opt_res = ready!(stream.as_mut().poll_next_py(py, cx));
         if let Some(res) = opt_res {
             if this.close {
-                ref_mut.0 = None;
+                *guard = None;
             }
             return Poll::Ready(res);
         }
-        ref_mut.0 = None;
+        *guard = None;
         Poll::Ready(err())
-    }
-}
-
-enum StreamOrNext {
-    Stream(Pin<Box<dyn PyStream>>),
-    Next(Py<StreamNext>),
-    // temporary used to switch between variants
-    Tmp,
-}
-
-impl StreamOrNext {
-    fn next<'a>(&'a mut self, py: Python<'a>) -> PyResult<&'a mut Py<StreamNext>> {
-        if matches!(self, Self::Stream(_)) {
-            let Self::Stream(stream) = mem::replace(self, StreamOrNext::Tmp) else {
-                unreachable!();
-            };
-            *self = Self::Next(Py::new(py, StreamNext(Some(stream)))?);
-        }
-        match self {
-            Self::Next(next) => Ok(next),
-            _ => unreachable!(),
-        }
     }
 }
 
@@ -65,7 +42,7 @@ pub(crate) trait CoroutineFactory {
 }
 
 pub(crate) struct AsyncGenerator<C> {
-    stream_or_next: StreamOrNext,
+    stream: SharedStream,
     throw: Option<ThrowCallback>,
     _phantom: PhantomData<C>,
 }
@@ -73,7 +50,7 @@ pub(crate) struct AsyncGenerator<C> {
 impl<C> AsyncGenerator<C> {
     pub(crate) fn new(stream: Pin<Box<dyn PyStream>>, throw: Option<ThrowCallback>) -> Self {
         Self {
-            stream_or_next: StreamOrNext::Stream(stream),
+            stream: Arc::new(Mutex::new(Some(stream))),
             throw,
             _phantom: PhantomData,
         }
@@ -82,8 +59,8 @@ impl<C> AsyncGenerator<C> {
 
 impl<C: CoroutineFactory> AsyncGenerator<C> {
     pub(crate) fn _next(&mut self, py: Python, close: bool) -> PyResult<PyObject> {
-        let next = self.stream_or_next.next(py)?.clone();
-        Ok(C::coroutine(PyStreamNext { next, close }).into_py(py))
+        let stream = self.stream.clone();
+        Ok(C::coroutine(PyStreamNext { stream, close }).into_py(py))
     }
 
     pub(crate) fn next(&mut self, py: Python) -> PyResult<PyObject> {
