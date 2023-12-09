73b04bd5c7061b45dff958ed422f12058c3d8352
Joseph Perez <joseph.perez@numberly.com>
2023-12-10 00:06:24 +0100

    fix: fix AllowThreads implementation

--- src/allow_threads.rs
+++ src/allow_threads.rs
@@ -6,6 +6,7 @@ use std::{
 
 use futures::Stream;
 use pin_project::pin_project;
+use pyo3::Python;
 
 /// Wrapper for [`Future`]/[`Stream`] that releases GIL while polling in
 /// [`PyFuture`](crate::PyFuture)/[`PyStream`](crate::PyStream).
@@ -18,21 +19,33 @@ use pin_project::pin_project;
 #[pin_project]
 pub struct AllowThreads<T>(#[pin] pub T);
 
-impl<F: Future> Future for AllowThreads<F> {
+impl<F> Future for AllowThreads<F>
+where
+    F: Future + Send,
+    F::Output: Send,
+{
     type Output = F::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
-        this.0.poll(cx)
+        let waker = cx.waker();
+        Python::with_gil(|gil| gil.allow_threads(|| this.0.poll(&mut Context::from_waker(waker))))
     }
 }
 
-impl<S: Stream> Stream for AllowThreads<S> {
+impl<S> Stream for AllowThreads<S>
+where
+    S: Stream + Send,
+    S::Item: Send,
+{
     type Item = S::Item;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
-        this.0.poll_next(cx)
+        let waker = cx.waker();
+        Python::with_gil(|gil| {
+            gil.allow_threads(|| this.0.poll_next(&mut Context::from_waker(waker)))
+        })
     }
 }
 
